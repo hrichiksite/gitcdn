a2d20a05d5af7cd2c354b4244cf730c99ae69d9d
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-12-19 23:09:56 +0530

    Update [path].js

--- api/[git]/[repo]/[path].js
+++ api/[git]/[repo]/[path].js
@@ -3,19 +3,15 @@ const fetch = require('node-fetch');
 module.exports = async (request, response) => {
       response.setHeader("Access-Control-Allow-Headers", '*');
       response.setHeader("Access-Control-Allow-Origin", '*');
+          response.setHeader("content-type", 'application/javascript');
+
   var user = request.query.git;
   var repo  = request.query.repo;
   var path = request.query.path;
   
   
-  regex = new RegExp('[^.]+$');
-  extension = path.match(regex);
 
-  if(extension=== "json"){
-    response.setHeader("content-type", 'application/json');
-  } else if(extension==="js"){
-    response.setHeader("content-type", 'application/javascript');
-  }
+
   var url = "https://raw.githubusercontent.com/" + user + "/"+ repo +"/master/"+ path
 
 
