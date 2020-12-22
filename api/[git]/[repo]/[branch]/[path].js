e88d1cd402c1119fdf08c9c9cdc078f565ae37b3
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-12-22 23:44:43 +0530

    Update [path].js

--- api/[git]/[repo]/[branch]/[path].js
+++ api/[git]/[repo]/[branch]/[path].js
@@ -1,25 +1,51 @@
 const fetch = require('node-fetch');
 
+//code from https://stackoverflow.com/questions/190852/how-can-i-get-file-extensions-with-javascript
+function getExtension(path) {
+    var basename = path.split(/[\\/]/).pop(),  // extract file name from full path ...
+                                               // (supports `\\` and `/` separators)
+        pos = basename.lastIndexOf(".");       // get last position of `.`
+
+    if (basename === "" || pos < 1)            // if file name is empty or ...
+        return "";                             //  `.` not found (-1) or comes first (0)
+
+    return basename.slice(pos + 1);            // extract extension ignoring `.`
+}
+
 module.exports = async (request, response) => {
       response.setHeader("Access-Control-Allow-Headers", '*');
       response.setHeader("Access-Control-Allow-Origin", '*');
-          response.setHeader("content-type", 'application/javascript');
 
   var user = request.query.git;
   var repo  = request.query.repo;
   var path = request.query.path;
   var branch = request.query.branch;
-  
-
+  var filetype = getExtension(path);
 
   var url = "https://raw.githubusercontent.com/" + user + "/"+ repo +"/"+ branch +"/"+ path
 
-
-
-
     var req = await fetch(url);
     var res = await req.text();
-    response.send(res);
+
+if(filetype===js){
+                response.setHeader("content-type", 'text/javascript');
+} else if(filetype===json){
+                response.setHeader("content-type", 'application/json');
+} else if(filetype===css){
+                response.setHeader("content-type", 'text/css');
+} else if(filetype===png){
+    response.send("Fetch This File Directly From GitHub, can't waste bandwidth");
+} else {
+    response.send("Sorry, File not supported");
+}
+
+
+
+
+
 
   
 }
+
+
+
