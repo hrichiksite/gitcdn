5457c461d718efee1794946dc391db846b1c1f0d
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2021-01-14 10:43:23 +0530

    Added "jquery", "Wruczek" (Bootstrap Cookie), "twbs"(Bootstrap)

--- api/gh/beta.js
+++ api/gh/beta.js
@@ -20,7 +20,7 @@ module.exports = async (request, response) => {
   var path = request.query.path;
   var branch = request.query.branch;
   var filetype = getExtension(path);
- var  whitelist = ["hrichiksite", "MatejaSrejic"];
+ var  whitelist = ["hrichiksite", "MatejaSrejic", "jquery", "Wruczek", "twbs"];
 
   var url = "https://raw.githubusercontent.com/" + user + "/"+ repo +"/"+ branch +"/"+ path;
 
